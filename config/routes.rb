5795cf2645c3ea3d0d310db277cf1ee94a86f11b
Jed <codingjv@gmail.com>
2016-04-04 18:30:02 +0000

    add stocks and ajax for UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   #get 'welcome' => 'welcome#index'
-  root 'welcome#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'welcome#index'
+  get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
